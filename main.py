d193f492217c4f60a4152fc962bd18d55fff9106
Matt Hunt <mstephenhunt@gmail.com>
2018-08-07 06:23:39 -0400

    started main, running simple loop printing deg f

---
+++ main.py
@@ -0,0 +1,22 @@
+import time
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MAX31855.MAX31855 as MAX31855
+
+clk_pin = 17
+cs_pin = 27
+do_pin = 22
+
+
+def c_to_f(c):
+    return c * 9.0 / 5.0 + 32.0
+
+
+if __name__ == '__main__':
+    sensor = MAX31855.MAX31855(clk_pin, cs_pin, do_pin)
+
+    while (True):
+        c_temp = sensor.readTempC()
+        f_temp = c_to_f(c_temp)
+
+        print(f_temp + " F")
+        time.sleep(0.1)
