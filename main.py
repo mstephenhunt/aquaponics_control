a0a005b563fb2c74b78659c5caa5920312a0cbc0
Matt Hunt <mstephenhunt@gmail.com>
2018-08-07 06:58:23 -0400

    formatting timestamp

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ if __name__ == '__main__':
         c_temp = sensor.readTempC()
         f_temp = c_to_f(c_temp)
 
-        print(c_temp)
-
-        print("[" + str(datetime.now(eastern)) + "] " + str(f_temp) + " F (" + str(c_temp) + " C)")
+        formatted_timestamp = datetime.now(eastern).strftime('%Y-%m-%d %H:%M:%S')
+        print("[" + formatted_timestamp + "] " + str(f_temp) + " F (" + str(c_temp) + " C)")
         time.sleep(0.25)
