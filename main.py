aae1ba4ef671d1ee7f6f729d0556163ce27a3c25
Matt Hunt <mstephenhunt@gmail.com>
2018-08-07 06:54:45 -0400

    formatting timestamp to est

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import time
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MAX31855.MAX31855 as MAX31855
-import datetime
+from datetime import datetime
+from pytz import timezone
 
 clk_pin = 17
 cs_pin = 27
 do_pin = 22
 
+eastern = timezone('US/Eastern')
 
 def c_to_f(c):
     return c * 9.0 / 5.0 + 32.0
@@ -21,5 +23,5 @@ if __name__ == '__main__':
 
         print(c_temp)
 
-        print("[" + str(datetime.datetime.now()) + "] " + str(f_temp) + " F (" + str(c_temp) + " C)")
+        print("[" + str(datetime.now(eastern)) + "] " + str(f_temp) + " F (" + str(c_temp) + " C)")
         time.sleep(0.25)
