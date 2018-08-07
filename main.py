7b0b213ff1dc084428b7404e9037210f28c2cd63
Matt Hunt <mstephenhunt@gmail.com>
2018-08-07 07:09:14 -0400

    removed c_temp

--- main.py
+++ main.py
@@ -26,15 +26,14 @@ if __name__ == '__main__':
     log_file = open("temp_log.txt", "w")
 
     while (True):
-        # c_temp = sensor.readTempC()
         # f_temp = c_to_f(c_temp)
 
         f_temp = get_probe_temperature(sensor)
 
         formatted_timestamp = datetime.now(eastern).strftime('%Y-%m-%d %H:%M:%S')
-        current_reading = "[" + formatted_timestamp + "] " + str(f_temp) + " F (" + str(c_temp) + " C)"
+        current_reading = "[" + formatted_timestamp + "] " + str(f_temp) + " F"
 
         print(current_reading)
-        log_file.write("[" + formatted_timestamp + "] " + str(f_temp) + " F (" + str(c_temp) + " C)\n")
+        log_file.write("[" + formatted_timestamp + "] " + str(f_temp) + " F (")
 
         time.sleep(0.25)
