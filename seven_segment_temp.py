8b788c99cee6ee82dded6794e381bd5aeb0d81cb
Matt Hunt <mstephenhunt@gmail.com>
2018-08-09 07:36:06 -0400

    moved DIGIT_VALUES up

--- seven_segment_temp.py
+++ seven_segment_temp.py
@@ -1,11 +1,11 @@
 import time
 from Adafruit_LED_Backpack import SevenSegment
 
-class SevenSegmentTemp:
-    DIGIT_VALUES = SevenSegment.DIGIT_VALUES
-    DIGIT_VALUES['U'] = 0x3e # 62
-    DIGIT_VALUES['T'] = 0x31 # 49
+DIGIT_VALUES = SevenSegment.DIGIT_VALUES
+DIGIT_VALUES['U'] = 0x3e # 62
+DIGIT_VALUES['T'] = 0x31 # 49
 
+class SevenSegmentTemp:
     def __init__ (self):
         self.display = SevenSegment.SevenSegment()
         self.display.begin()
