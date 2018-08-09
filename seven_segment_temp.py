ad3669d2e0975ee917f95824327823261954092c
Matt Hunt <mstephenhunt@gmail.com>
2018-08-09 07:33:43 -0400

    starting to convert message to hex digits for raw writing

--- seven_segment_temp.py
+++ seven_segment_temp.py
@@ -2,9 +2,20 @@ import time
 from Adafruit_LED_Backpack import SevenSegment
 
 class SevenSegmentTemp:
+    DIGIT_VALUES = SevenSegment.DIGIT_VALUES
+    DIGIT_VALUES['U'] = 0x3e # 62
+    DIGIT_VALUES['T'] = 0x31 # 49
+
     def __init__ (self):
         self.display = SevenSegment.SevenSegment()
         self.display.begin()
+
+    def __convert_message_to_digit_values(self, message):
+        converted_message = []
+        for char in message:
+            converted_message.push(DIGIT_VALUES.get(str(digit).upper(), 0x00))
+
+        return converted_message
 
 
     # This only accepts ints that have a length <= 3. The 
@@ -29,18 +40,19 @@ class SevenSegmentTemp:
         self.display.write_display()
 
 
-    # def marquee_message (self, message):
-    #     self.display.clear()
-    #     current_chars = []
+    # Currently only works with 0-9, A-F, U, and T
+    def marquee_message (self, message):
+        # Append four spaces to end of message so 
+        # that it goes fully off screen when finished
+        message_with_spaces = message + '    '
 
-    #     # Append four spaces to end of message so 
-    #     # that it goes fully off screen when finished
-    #     message_with_spaces = message + '    '
+        self.__convert_message_to_digit_values(message_with_spaces)
 
-    #     for char in message_with_spaces:
-    #         current_chars.append(char)
+        return message_with_spaces
+        # for char in message_with_spaces:
+        #     current_chars.append(char)
 
-    #         if (len(current_chars) > 4):
-    #             current_chars.pop[0]
+        #     if (len(current_chars) > 4):
+        #         current_chars.pop[0]
 
-    #         self.display
+        #     self.display
