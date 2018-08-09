3d33681a71f07aa297ef79eac9dc70bc977e179a
Matt Hunt <mstephenhunt@gmail.com>
2018-08-09 05:13:25 -0400

    created seven_segment_temp.py

---
+++ seven_segment_temp.py
@@ -0,0 +1,19 @@
+from Adafruit_LED_Backpack import SevenSegment
+
+class SevenSegmentTemp:
+    def __init__ (self):
+        self.display = SevenSegment.SevenSegment()
+        self.display.begin()
+
+    # This only accepts ints that have a length <= 3. The 
+    # fourth segment on the display shows the letter F
+    def display_temp (temp):
+        if (type(temp) is int and str(temp).length <= 3):
+            temp_string = str(temp)
+
+            self.display.clear()
+            self.display.print_number_str(temp_string)
+            self.display.write_display()
+
+        else:
+            return
