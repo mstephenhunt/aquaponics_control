06f34dfb8d5c5d9d47982b7b6a832bf59d81a54d
Matt Hunt <mstephenhunt@gmail.com>
2018-09-03 16:08:37 -0400

    continuious readings

--- temp_hack.py
+++ temp_hack.py
@@ -1,11 +1,15 @@
 from seven_segment import CustomSevenSegment
 from temp_logger import TempLogger
+import time
 
 # {'probe': 91.4, 'ambient': 92.075, 'time': '2018-09-03 16:02:28'}
 
 logger = TempLogger(1)
-temp_info = logger.get_temperature_readings()
-probe_temp = int(temp_info['probe'])
-
 display = CustomSevenSegment()
-display.display_temp(probe_temp)
+
+while (True):
+    time.sleep(0.25)
+
+    temp_info = logger.get_temperature_readings()
+    probe_temp = int(temp_info['probe'])
+    display.display_temp(probe_temp)
