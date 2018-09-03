2bfbbf59bd14e61e98758afa3b7052016884ec7a
Matt Hunt <mstephenhunt@gmail.com>
2018-09-03 16:06:30 -0400

    CustomSevenSegment

--- temp_hack.py
+++ temp_hack.py
@@ -1,4 +1,4 @@
-from seven_segment import SevenSegment
+from seven_segment import CustomSevenSegment
 from temp_logger import TempLogger
 
 # {'probe': 91.4, 'ambient': 92.075, 'time': '2018-09-03 16:02:28'}
@@ -7,5 +7,5 @@ logger = TempLogger(1)
 temp_info = logger.get_temperature_readings()
 probe_temp = int(temp_info['probe'])
 
-display = SevenSegment()
+display = CustomSevenSegment()
 display.display_temp(probe_temp)
