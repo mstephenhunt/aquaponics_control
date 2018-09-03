2faed3eeb0d3937b66e3a80e19084ffbe8a53fcd
Matt Hunt <mstephenhunt@gmail.com>
2018-09-03 16:04:40 -0400

    added temp_hack.py

---
+++ temp_hack.py
@@ -0,0 +1,10 @@
+from seven_segment import SevenSegment
+from temp_logger import TempLogger
+
+# {'probe': 91.4, 'ambient': 92.075, 'time': '2018-09-03 16:02:28'}
+
+logger = TempLogger(1)
+temp_info = logger.get_temperature_readings()
+probe_temp = int(temp_info['probe'])
+
+print(probe_temp)
