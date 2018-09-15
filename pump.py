7a4562a9b82f6c138ce217f0be056c15ae9d1aed
Matt Hunt <mstephenhunt@gmail.com>
2018-09-15 13:12:56 -0400

    created pump obj, extends relay_control

---
+++ pump.py
@@ -0,0 +1,11 @@
+from relay_control import RelayControl
+
+class Pump(RelayControl):
+    __init__(self, pin):
+        RelayControl.__init__(self, pin)
+        self.on_interval = 0
+        self.off_interval = 0
+
+    def set_on_off_intervals(self, on_interval, off_interval):
+        self.on_interval = on_interval
+        self.off_interval = off_interval
